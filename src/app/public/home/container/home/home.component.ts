7a18a1577e665b9554bb2ad7470307b0bc2d5061
manueljuan123 <79883680+manueljuan123@users.noreply.github.com>
2022-10-11 22:31:20 -0500

    Montenegro y carousel
    Se agregaron las fotos editadas del evento La Soledad, Montenegro y se cambiaron gifs con la finalidad de optimización de la página

--- src/app/public/home/container/home/home.component.ts
+++ src/app/public/home/container/home/home.component.ts
@@ -12,32 +12,37 @@ export class HomeComponent implements OnInit {
   images = [
     {
       imageSrc:
-        'assets/img/home/carousel/1armenia.gif',
+        'assets/img/home/carousel/1armenia.jpeg',
       imageAlt: 'Armenia'
     },
     {
       imageSrc:
-      'assets/img/home/carousel/2calarca.gif',
+      'assets/img/home/carousel/2calarca.jpeg',
       imageAlt: 'Calarcá'
     },
     {
       imageSrc:
-      'assets/img/home/carousel/3montenegro.jpeg',
+      'assets/img/home/carousel/3circasia.jpeg',
+      imageAlt: 'Circasia'
+    },
+    {
+      imageSrc:
+      'assets/img/home/carousel/4montenegro.jpeg',
       imageAlt: 'Montenegro'
     },
     {
       imageSrc:
-      'assets/img/home/carousel/4quimbaya.gif',
+      'assets/img/home/carousel/5quimbaya.jpeg',
       imageAlt: 'Quimbaya'
     },
     {
       imageSrc:
-      'assets/img/home/carousel/5pijao.jpeg',
+      'assets/img/home/carousel/6pijao.jpeg',
       imageAlt: 'Pijao'
     },
     {
       imageSrc:
-      'assets/img/home/carousel/6cordoba.jpeg',
+      'assets/img/home/carousel/7cordoba.jpeg',
       imageAlt: 'Córdoba'
     },
   ]
